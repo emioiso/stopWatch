07182bf453d149ca9ca13cc50e35db4d6af236cc
emioiso <jijjjiijiijijiji@gmail.com>
2023-11-01 12:54:14 +0700

    Initial Commit

---
+++ stopWatch/stopWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  stopWatchApp.swift
+//  stopWatch
+//
+//  Created by emi oiso on 2023/11/01.
+//
+
+import SwiftUI
+
+@main
+struct stopWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
