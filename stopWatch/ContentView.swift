6e2ce68a086c8f20389b57f3523783e1f9c6bbdd
emioiso <jijjjiijiijijiji@gmail.com>
2023-11-01 16:19:57 +0700

    stopWatchApp

--- stopWatch/ContentView.swift
+++ stopWatch/ContentView.swift
@@ -8,14 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var timer: Timer? = nil
+    @State var secondsElapsed: Double = 0.0
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                
+        ZStack{
+            Color("backgroungColor")
+                .ignoresSafeArea()
+            VStack {
+                
+                Text(String(format:"%.1f", secondsElapsed))
+                    .font(Font(UIFont.monospacedSystemFont(ofSize: 120, weight:.black)))
+                    
+                ButtonView(buttonAction: {start()}, text: "Start", color: .green)
+                ButtonView(buttonAction: {stop()}, text: "Stop", color: .red)
+                ButtonView(buttonAction: {reset()}, text: "Reset", color: .gray)  
+            }
         }
         .padding()
+    }
+    
+    
+    
+    func start() {
+        if timer == nil {
+            timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true){ _ in
+                secondsElapsed += 0.1
+            }
+        }
+    }
+    
+    func stop() {
+        if let time = timer {
+            time.invalidate()
+        }
+        timer = nil
+    }
+    
+    func reset() {
+        if let time = timer {
+            time.invalidate()
+        }
+        timer = nil
+        secondsElapsed = 0
+    }
+}
+
+struct ButtonView: View {
+    let buttonAction:() -> Void
+    let text: String
+    let color: Color
+    
+    var body: some View{
+        Button {
+            buttonAction()
+        } label: {
+            Text(text)
+                .font(.largeTitle)
+                .frame(width:200)
+                .foregroundColor(Color("buttonColor"))
+                .padding()
+        }
     }
 }
 
